2c80f2b4ec73d59e6047a40c0a26d4ec20e151c5
tfemerson <tf_emerson@hotmail.com>
2025-11-23 22:30:49 +0800

    准备生产环境部署

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,16 @@ export default defineConfig({
   plugins: [solid()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    // ✅ 添加代理配置，用于本地开发
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
   },
   build: {
     target: 'esnext'
   }
-}); 
+});
